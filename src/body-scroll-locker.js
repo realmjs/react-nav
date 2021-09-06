8d5c91f3049a1e33fda6975fdbec571e440a0b70
duongtdn <duongtdn@gmail.com>
2021-09-06 11:10:04 +0700

    Fix body-scroll is locked if open two popups

--- src/body-scroll-locker.js
+++ src/body-scroll-locker.js
@@ -1,4 +1,3 @@
-
 "use strict"
 
 export default function() {
@@ -7,16 +6,38 @@ export default function() {
 
   return {
 
+    _lockCnt: 0,
+
     enable() {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = originalBodyStyle;
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+      if (this._lockCnt === 1) {
+        const scrollY = document.body.style.top;
+        this.restoreBodyStyle();
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        this.isDisabled = false;
+      }
+      if (this._lockCnt > 0) {
+        this._lockCnt--;
+      } else {
+        this._lockCnt = 0;
+      }
     },
 
     disable() {
-      originalBodyStyle = document.body.style.cssText;
-      document.body.style.cssText = `; overflow: hidden; position: fixed;  width: 100%; top: -${window.scrollY}px `;
+      if (this._lockCnt === 0) {
+        this.backupBodyStyle();
+        document.body.style.cssText = `; overflow: hidden; position: fixed;  width: 100%; top: -${window.scrollY}px `;
+        this.isDisabled = true;
+      }
+      this._lockCnt++;
     },
+
+    backupBodyStyle() {
+      originalBodyStyle = document.body.style.cssText;
+    },
+
+    restoreBodyStyle() {
+      document.body.style.cssText = originalBodyStyle;
+    }
 
   }
 }
